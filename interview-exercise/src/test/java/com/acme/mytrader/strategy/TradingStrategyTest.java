f362131d91c36806ba690831f70a0b5ae5c5d8b1
arvindraje77 <patil.arvind77@gmail.com>
2022-11-14 15:14:12 +0000

    Added Test Case

--- interview-exercise/src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
+++ interview-exercise/src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
@@ -1,9 +1,40 @@
 package com.acme.mytrader.strategy;
 
-import org.junit.Test;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.acme.mytrader.execution.ExecutionService;
+import com.acme.mytrader.price.PriceSource;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TradingStrategyTest {
+
+    TradingStrategy tradingStrategy;
+    @Mock PriceSource priceSource;
+    @Mock ExecutionService broker;
+    
+    @Before
+    public void init() {
+        tradingStrategy = new TradingStrategy(broker, 100);
+        tradingStrategy.setBuyer("BT", 55.0);
+        tradingStrategy.setSeller("BT", 550.0);
+    }
+    
     @Test
-    public void testNothing() {
+    public void testBuyRationale() {
+        tradingStrategy.priceUpdate("BT", 56.0);
+
+        Mockito.verify(broker, Mockito.times(0)).buy(anyString(), anyDouble(), anyInt());
+        tradingStrategy.priceUpdate("BT", 54.0);
+
+        Mockito.verify(broker, Mockito.times(1)).buy("IBM", 54.0, 100);
     }
 }
